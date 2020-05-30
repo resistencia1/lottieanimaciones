05d2304a09570e9180eb80988f13d98113d275b2
resistencia1 <crisevil1@gmail.com>
2020-05-30 18:56:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lottieanimaciones/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lottieanimaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isSwitchOn =false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LottieAnimationView  lottieSwitchButton=findViewById(R.id.lottSwitch);
+        //cambiar velocidad
+        lottieSwitchButton.setSpeed(3f);
+        lottieSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isSwitchOn){
+                    lottieSwitchButton.setMinAndMaxProgress(0.5f,1.0f);
+                    lottieSwitchButton.playAnimation();
+                    isSwitchOn=false;
+                }else{
+                    lottieSwitchButton.setMinAndMaxProgress(0.0f,0.5f);
+                    lottieSwitchButton.playAnimation();
+                    isSwitchOn=true;
+                }
+            }
+        });
+
+    }
+}
