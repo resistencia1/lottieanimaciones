59a90abf662fb74e6202515c29940be83eda646b
resistencia1 <crisevil1@gmail.com>
2020-05-30 19:13:06 -0500

    fdsadf

--- app/src/main/java/com/example/lottieanimaciones/MainActivity.java
+++ app/src/main/java/com/example/lottieanimaciones/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         //cambiar velocidad
         //&dsafds
         //&dsafds
+        //&dsafds
         //&dsafdszfczxczx
         //&dsafds
         lottieSwitchButton.setSpeed(3f);
